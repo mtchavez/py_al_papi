721b159104082d703bf738e104e33aafc31fab58
Chavez <matthew.chavez@authoritylabs.com>
2012-03-14 14:32:14 -0700

    Initial package setup.

---
+++ setup.py
@@ -0,0 +1,15 @@
+import al_papi
+from distutils.core import setup
+setup(
+    name="al_papi",
+    version=al_papi.version.Version,
+    description="AuthorityLabs",
+    author="Chavez",
+    author_email="support@authoritylabs.com",
+    url="http://www.api.authoritylabs.com",
+    packages=["al_papi"],
+    package_data={},
+    install_requires=[],
+    tests_require=[],
+    zip_safe=False
+)
