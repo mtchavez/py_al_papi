e47bd3225158a7504d1353bb596a9521784ec327
Chavez <matthew.chavez@authoritylabs.com>
2012-03-16 22:44:25 -0600

    Fix locales test to return correct array.

--- tests/unit/test_locales.py
+++ tests/unit/test_locales.py
@@ -3,10 +3,10 @@ from tests.test_helper import *
 class TestLocales(unittest.TestCase):
   
   def test_google_locales(self):
-    assert_equal(Locales.supported(), Locales.google)
+    assert_equal(Locales.supported(), Locales.google())
   
   def test_yahoo_locales(self):
-    assert_equal(Locales.supported("yahoo"), Locales.yahoo)
+    assert_equal(Locales.supported("yahoo"), Locales.yahoo())
   
   def test_bing_locales(self):
-    assert_equal(Locales.supported("bing"), Locales.bing)
+    assert_equal(Locales.supported("bing"), Locales.bing())
