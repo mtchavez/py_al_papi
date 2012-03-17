23db60d716382cba12ee06e4ff0a780422a7de1b
Chavez <matthew.chavez@authoritylabs.com>
2012-03-16 22:34:39 -0600

    Added locales class with test file.

--- al_papi/__init__.py
+++ al_papi/__init__.py
@@ -1,3 +1,7 @@
+from restclient import GET, POST, PUT, DELETE
+
 from config  import Config
 from engines import Engines
+from locales import Locales
+from request import Request
 from version import Version
---
+++ tests/unit/test_locales.py
@@ -0,0 +1,12 @@
+from tests.test_helper import *
+
+class TestLocales(unittest.TestCase):
+  
+  def test_google_locales(self):
+    assert_equal(Locales.supported(), Locales.google)
+  
+  def test_yahoo_locales(self):
+    assert_equal(Locales.supported("yahoo"), Locales.yahoo)
+  
+  def test_bing_locales(self):
+    assert_equal(Locales.supported("bing"), Locales.bing)
